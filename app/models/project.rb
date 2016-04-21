6c3840335eb61fc2f3c1a4216b741ae658e52b04
jvssvarma <varma.santosh@outlook.com>
2016-04-20 20:32:49 -0700

    increase form limit

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ActiveRecord::Base
   belongs_to :user
-  validates :title, presence: true, length: {minimum: 2, maximum: 50}
-  validates :description, presence: true, length: {minimum: 10, maximum:600}
+  validates :title, presence: true, length: {minimum: 2, maximum: 200}
+  validates :description, presence: true, length: {minimum: 10, maximum:10000}
   validates :user_id, presence: true
 end
