c8cbb4283d0e9dfc8b798b88118807bbb5a123cf
jvssvarma <varma.santosh@outlook.com>
2016-04-24 17:48:33 -0700

    Add Categorries model and tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160421013236) do
+ActiveRecord::Schema.define(version: 20160421045644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
